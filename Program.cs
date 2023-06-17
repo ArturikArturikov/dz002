d83f86907fed77859e0e7566fa4db7687455cb3b
Artur <ivan_ivankov@mail.ru>
2023-06-17 21:11:16 +0300

    2 и 3 задача

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
 
-int Prompt(string message)
+/* int Prompt(string message)
 {
     Console.Write(message);
     string value = Console.ReadLine()!;
@@ -16,4 +16,85 @@ if (number < 100 || number >= 1000)
     return;
 }
 
-Console.WriteLine(seconRank);
+Console.WriteLine(seconRank); */
+
+// ___________________________________________________________________________________________________________________________________________________________________
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/* int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()!;
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int GetThirdRank(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+return true;
+}
+
+int number = Prompt("Введите число: ");
+if (ValidateNumber(number));
+{
+    Console.WriteLine(GetThirdRank(number));
+} */
+
+// _________________________________________________________________________________________________________________________________________________________________
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет является ли этот день выходным.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()!;
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekday(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели!");
+    return false;
+}
+
+int weekDay = Prompt("Введите день недели: ");
+if (ValidateWeekday(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день");
+    }
+}
